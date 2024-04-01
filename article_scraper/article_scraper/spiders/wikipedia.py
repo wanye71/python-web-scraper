20c9c7f9d719b833abf3c15f0011caeb437b9201
wayne71 <wayne.hatter1971@gmail.com>
2024-04-01 11:28:06 -0500

    wiki scraper

---
+++ article_scraper/article_scraper/spiders/wikipedia.py
@@ -0,0 +1,17 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class WikipediaSpider(CrawlSpider):
+    name = "wikipedia"
+    allowed_domains = ["en.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/Wallace_Wattles"]
+
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
+    def parse(self, response):
+        return {
+            "title": response.xpath('//h1/text()').get() or response.xpath('//h1/i/text').get(),
+            "url": response.url,
+            'last_edited':response.xpath('//li[@id="footer-info-lastmod"]/textt()').get()
+        }
