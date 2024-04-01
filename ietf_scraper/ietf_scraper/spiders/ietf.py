03c0272ea92022b047c4793496e69559929887ec
wayne71 <wayne.hatter1971@gmail.com>
2024-04-01 10:59:11 -0500

    tags object

--- ietf_scraper/ietf_scraper/spiders/ietf.py
+++ ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -7,6 +7,13 @@ class IetfSpider(scrapy.Spider):
     start_urls = ["https://pythonscraping.com/linkedin/ietf.html"]
 
     def parse(self, response):
-       # title = response.css('span.title::text').get()
-        title = response.xpath('//span[@class="title"]/text()').get()
-        return {"title": title}
+       return {
+           "schema": response.xpath('//link[@rel="schema.DC"]/@href').get(),
+           "author": response.xpath('//span[@class="author-name"]/text()').get(),
+           "rfc-number": response.xpath('//span[@class="rfc-no"]/text()').get(),
+           "author-co": response.xpath('//span[@class="author-company"]/text()').get(),
+           "date": response.xpath('//span[@class="date"]/text()').get(),
+           "title": response.xpath('//span[@class="title"]/text()').get(),
+           "address": response.xpath('//span[@class="address"]/text()').get(),
+           "headings": response.xpath('//span[@class="subheading"]/text()').get(),
+       }
