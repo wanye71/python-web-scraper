37cedde47959fa4eddc7be7c0c3b4d6086df6e5d
wayne71 <wayne.hatter1971@gmail.com>
2024-04-01 11:57:50 -0500

    crawler to csv

--- article_scraper/article_scraper/items.py
+++ article_scraper/article_scraper/items.py
@@ -6,7 +6,11 @@
 import scrapy
 
 
-class ArticleScraperItem(scrapy.Item):
+class Article(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    title = scrapy.Field()
+    url = scrapy.Field()
+    lastUpdated = scrapy.Field()
+    
--- article_scraper/article_scraper/spiders/wikipedia.py
+++ article_scraper/article_scraper/spiders/wikipedia.py
@@ -1,17 +1,20 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
+from article_scraper.items import Article
 
 class WikipediaSpider(CrawlSpider):
     name = "wikipedia"
     allowed_domains = ["en.wikipedia.org"]
-    start_urls = ["https://en.wikipedia.org/wiki/Wallace_Wattles"]
+    start_urls = ["https://en.wikipedia.org/wiki/"]
 
-    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
-    def parse(self, response):
-        return {
-            "title": response.xpath('//h1/text()').get() or response.xpath('//h1/i/text').get(),
-            "url": response.url,
-            'last_edited':response.xpath('//li[@id="footer-info-lastmod"]/textt()').get()
-        }
+    rules = [
+        Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)
+        ]
+    def parse_info(self, response):
+        article = Article()
+        article["title"] = response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get()
+        article["url"] = response.url
+        article['lastUpdated'] = response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+        return article
+        
